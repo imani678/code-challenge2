d11d1a2892cc8b228490d663fc9925901b7ad5e8
imani <18imani.vata@gmail.com>
2024-04-02 16:56:09 +0300

    done

--- Q3.js
+++ Q3.js
@@ -1,9 +1,25 @@
 //function that takes an array of numbers and returns only prime numbers from the original array
 //defining our function
 
-function genPrime(){
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9,]
-for (let i = 2; i <= limit; i++) 
+function genPrime(numbers) {
 
-return array}
-    console.log (genPrime)
+    let emptyArray = [];
+    for (let i = 0; i < numbers.length; i++) {
+      let prime = true;
+      
+      for (let j = 2; j < numbers[i]; j++) {
+        if (numbers[i] % j === 0) {
+          prime = false;
+          break; 
+        }}
+
+
+      if (prime && numbers[i] > 1) {
+       
+        emptyArray.push(numbers[i]);
+      }
+    }
+    return emptyArray; 
+  }
+  
+  console.log(genPrime([1, 2, 3, 4, 5, 6, 7, 8, 9]));
